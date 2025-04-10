f24b64425bd7cbff65da735302ef8d3a17a15bd7
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-10 17:06:46 -0300

    criando models

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudante(models.Model):
+    
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, blank=False)
+    cpf = models.CharField(max_length=11, unique=True)
+    data_nascimento = models.DateField()
+    numero_celular = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.nome
+    
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+    codigo = models.CharField(max_length=10, unique=True)
+    descricao = models.CharField(max_length=255, blank=False)
+    nivel = models.CharField(max_length=1, choices=NIVEL, null=False, blank=False, default='B')
+    
+    def __str__(self):
+        return self.codigo
+    
