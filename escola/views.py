78c49807a38fa81b718be48eadb4b4a6107fc19d
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-10 18:29:24 -0300

    testando PU PATCH E DELETE

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
+from escola.models import Estudante, Curso
+from escola.serializers import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response    
 
-def estudantes(request):
-    if request.method == 'GET':
-        estudante = {
-            'id':'1',
-            'nome':'Alex'
-        }
-        return JsonResponse(estudante)
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+    
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
